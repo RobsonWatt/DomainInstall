dbd789c24e0818b01caf010c3895cbf98dfe623e
Robson Watt <rwattcha@rwattcha.redhat.com>
2018-07-02 23:12:24 -0400

    version 2

--- Common-Tasks/roles/eap-domain-controller-create-new-groups/tasks/create-groups.sh
+++ Common-Tasks/roles/eap-domain-controller-create-new-groups/tasks/create-groups.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 #set -x
 
+echo $1
+echo $2
+echo $3
+
 IFS='|' read -r -a grupo <<< $1
 profile=${grupo[1]}
 nombre=${grupo[0]}
-socket=${grupo[2]}
+socket_binding=${grupo[2]}
 
+nombre=$(echo $nombre | sed -e "s/\[//")
+socket_binding=$(echo $socket_binding | sed -e "s/\]//")
 
-$2/bin/jboss-cli.sh -c controller=$3:9999 --command="/server-group=$nombre:add(profile=$profile,socket-binding-group=$socket"
+echo $nombre
+echo $socket_binding
+
+$2/bin/jboss-cli.sh -c controller=$3:9999 --command="/server-group=$nombre:add(profile=$profile,socket-binding-group=$socket_binding)"
 $2/bin/jboss-cli.sh -c controller=$3:9999 --command="/server-group=$nombre/jvm=default:add()"
